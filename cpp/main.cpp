95c8aee9ea804c1b94ca00758c2cffe250d706fc
bbangsik13 <78462588+bbangsik13@users.noreply.github.com>
2021-07-30 12:10:57 +0900

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -9,7 +9,7 @@
 
 
 
-/**************************[ queue ∞¸∑√ ]*******************************/
+/**************************[ queue Í¥ÄÎ†® ]*******************************/
 struct WS_struct
 {
 public:
@@ -34,114 +34,11 @@ bool operator<(const WS_struct& a, const WS_struct& b)
     return a.dist > b.dist;
 }
 
-/**************************[ gredient ±∏«ˆ ]*******************************/
 
-cv::Mat gredient(cv::Mat arr) {
-    int width = arr.cols;
-    int height = arr.rows;
-
-
-    cv::Mat diff_img = cv::Mat::zeros(width, height, CV_32F);
-    /*
-    // diff_img[X or Y ∞° 0¿Œ ∫Œ∫–] <= arr[X or Y]
-    for (int x = 0; x < width; x++) {
-        diff_img.at<float>(x, 0) = arr.at<uchar>(x, 0);
-    }
-    for (int y = 1; y < height; y++) {
-        diff_img.at<float>(0, y) = arr.at<uchar>(0, y);
-    }
-
-    //diff_img [x] = root( (arr[x]-arr[x-1])^2 + (arr[y]-arr[y-1])^2 )
-    for (int y = 1; y < height; y++) {
-        for (int x = 1; x < width; x++) {
-            diff_img.at<float> (x, y) =
-                sqrtf(
-                    powf((arr.at<uchar>(x,y) - arr.at<uchar>(x-1, y))/2.0, 2.0) +
-                    powf((arr.at<uchar>(x,y) - arr.at<uchar>(x, y-1))/2.0, 2.0)
-                );
-        }
-    }
-    */
-
-
-    // diff_img[X or Y ∞° 0¿Œ ∫Œ∫–] <= arr[X or Y]
-
-    for (int x = 1; x < width - 1; x++) {
-        diff_img.at<float>(x, 0) =
-            sqrtf(
-                powf((arr.at<uchar>(x + 1, 0) - arr.at<uchar>(x - 1, 0)) / 2.0, 2.0) +
-                powf((arr.at<uchar>(x, 1) - arr.at<uchar>(x, 0)), 2.0)
-            );
-        diff_img.at<float>(x, height - 1) =
-            sqrtf(
-                powf((arr.at<uchar>(x + 1, height - 1) - arr.at<uchar>(x - 1, height - 1)) / 2.0, 2.0) +
-                powf((arr.at<uchar>(x, height - 1) - arr.at<uchar>(x, height - 2)), 2.0)
-            );
-    }
-    for (int y = 1; y < height - 1; y++) {
-        diff_img.at<float>(0, y) =
-            sqrtf(
-                powf((arr.at<uchar>(1, y) - arr.at<uchar>(0, y)), 2.0) +
-                powf((arr.at<uchar>(0, y + 1) - arr.at<uchar>(0, y - 1)) / 2.0, 2.0)
-            );
-        diff_img.at<float>(width - 1, y) =
-            sqrtf(
-                powf((arr.at<uchar>(width - 1, y) - arr.at<uchar>(width - 2, y)), 2.0) +
-                powf((arr.at<uchar>(width - 1, y + 1) - arr.at<uchar>(width - 1, y - 1)) / 2.0, 2.0)
-            );
-    }
-
-    diff_img.at<float>(0, 0) =
-        sqrtf(
-            powf((arr.at<uchar>(1, 0) - arr.at<uchar>(0, 0)), 2.0) +
-            powf((arr.at<uchar>(0, 1) - arr.at<uchar>(0, 0)), 2.0)
-        );
-    diff_img.at<float>(0, height - 1) =
-        sqrtf(
-            powf((arr.at<uchar>(1, height - 1) - arr.at<uchar>(0, height - 1)), 2.0) +
-            powf((arr.at<uchar>(0, height - 1) - arr.at<uchar>(0, height - 2)), 2.0)
-        );
-    diff_img.at<float>(width - 1, 0) =
-        sqrtf(
-            powf((arr.at<uchar>(width - 1, 0) - arr.at<uchar>(width - 2, 0)), 2.0) +
-            powf((arr.at<uchar>(width - 1, 1) - arr.at<uchar>(width - 1, 0)), 2.0)
-        );
-    diff_img.at<float>(width - 1, height - 1) =
-        sqrtf(
-            powf((arr.at<uchar>(width - 1, height - 1) - arr.at<uchar>(width - 2, height - 1)), 2.0) +
-            powf((arr.at<uchar>(width - 1, height - 1) - arr.at<uchar>(width - 1, height - 2)), 2.0)
-        );
-
-    //diff_img [x] = root( (arr[x]-arr[x-1])^2 + (arr[y]-arr[y-1])^2 )
-    for (int y = 1; y < height - 1; y++) {
-        for (int x = 1; x < width - 1; x++) {
-            diff_img.at<float>(x, y) =
-                sqrtf(
-                    powf((arr.at<uchar>(x + 1, y) - arr.at<uchar>(x - 1, y)) / 2.0, 2.0) +
-                    powf((arr.at<uchar>(x, y + 1) - arr.at<uchar>(x, y - 1)) / 2.0, 2.0)
-                );
-        }
-    }
-
-
-
-
-    /*
-    cv::Mat Sobal_x;
-    cv::Mat Sobal_y;
-
-    cv::Sobel(arr, Sobal_x, CV_8UC1, 1, 0, 3, 1, 128);
-    cv::Sobel(arr, Sobal_y, CV_8UC1, 0, 1, 3, 1, 128);
-    diff_img = abs(Sobal_x-128) + abs(Sobal_y-128);
-    */
-
-    return diff_img;
-}
-
-/*************************[ add«‘ºˆ(queue push)±∏«ˆ ]********************************/
+/*************************[ addÌï®Ïàò(queue push)Íµ¨ÌòÑ ]********************************/
 void add_neighbor(std::priority_queue<WS_struct>& WS_que, int marker, int x, int y, cv::Mat img, cv::Mat tag) {
     if (x - 1 >= 0) {//add left
-        if (!tag.at<uchar>(x - 1, y)) {//tag æ» µ«æÓ ¿÷¿∏∏È queue ≥÷±‚ (dist, x, y, marker)
+        if (!tag.at<uchar>(x - 1, y)) {//tag Ïïà ÎêòÏñ¥ ÏûàÏúºÎ©¥ queue ÎÑ£Í∏∞ (dist, x, y, marker)
             WS_struct wst(abs(img.at<uchar>(x, y)-img.at<uchar>(x - 1, y)), x - 1, y, marker);
             WS_que.push(wst);
         }
@@ -172,7 +69,7 @@ void add_neighbor(std::priority_queue<WS_struct>& WS_que, int marker, int x, int
 
 /*********************************************************/
 
-//tag ∫ÛπËø≠
+//tag ÎπàÎ∞∞Ïó¥
 
 //S priority queue          
 
@@ -181,14 +78,14 @@ cv::Mat watershed(cv::Mat img_gray, cv::Mat tag) {
     std::priority_queue<WS_struct> WS_que;
 
 
-    //¿¸√≥∏Æ..?
+    //Ï†ÑÏ≤òÎ¶¨..?
 
     //cv::GaussianBlur(img_grad, img_grad, cv::Size(3,3), 10.0);
 
 
 
 
-    // ¿”Ω√ √ ±‚ ∂Û∫ß∏µ
+    // ÏûÑÏãú Ï¥àÍ∏∞ ÎùºÎ≤®ÎßÅ
 
     /**********/
     WS_struct black(float(0), 306, 306, 80);
@@ -202,14 +99,14 @@ cv::Mat watershed(cv::Mat img_gray, cv::Mat tag) {
 
     /**********/
     int test_cnt = 0;
-    while (!WS_que.empty()) {//Sø° ¿÷¥¬ ¡÷∫Ø∫Œ »Æ¿Â
+    while (!WS_que.empty()) {//SÏóê ÏûàÎäî Ï£ºÎ≥ÄÎ∂Ä ÌôïÏû•
         WS_struct wst = WS_que.top(); //class
         WS_que.pop();
         int x = wst.x;
         int y = wst.y;
         int marker = wst.marker;
 
-        if (!tag.at<uchar>(x, y)) { //πÃ¡§ tag marker∑Œ ∏∂≈∑
+        if (!tag.at<uchar>(x, y)) { //ÎØ∏Ï†ï tag markerÎ°ú ÎßàÌÇπ
             tag.at<uchar>(x, y) = marker;
             add_neighbor(WS_que, marker, x, y, img_gray, tag);
         }
